61bf15241033fe6306b24b923d9f2ebd21aadbe4
rahavi3011 <103027160+rahavi3011@users.noreply.github.com>
2023-03-30 17:02:51 +0530

    Add files via upload

---
+++ AdditionExample.java
@@ -0,0 +1,21 @@
+class AdditionExample
+{
+public static void main(String[] arg)//predefined method 
+{
+//declare variable
+//datatype variablename;
+int num1=10,num2=20,num3;//garbage value concept 
+
+num3=num1+num2;
+
+System.out.println(num1+" + "+num2+" = "+num3);
+
+int num4=10,num5=20,num6;//garbage value concept 
+
+num6=num4+num5;
+
+System.out.println(num4+" + "+num5+" = "+num6);
+
+
+}
+}
